c23889c04153063c5b8a282aaa7c3a7a3a48e800
Louise Verbeke <louiseverbeke@yahoo.be>
2026-04-24 13:03:30 -0400

    Initial Commit

---
+++ Rooted/RootedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootedApp.swift
+//  Rooted
+//
+//  Created by Louise Verbeke on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RootedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
